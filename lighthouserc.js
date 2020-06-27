610d3a965f2e16ca4a3ce91021b1e966e02f5ca6
plug-n-play <>
2020-06-27 14:27:01 +0530

    c

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ["http://localhost:3000/"],
-      startServerCommand: "npm start",
+      startServerCommand: "npm install && npm start",
     },
     upload: {
       target: "temporary-public-storage",
